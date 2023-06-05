912924228054a838b35f63f7c86363e2280872fa
RiffLord <bruno92@duck.com>
2023-06-05 15:50:25 +0200

    Array sorting algorithm implementation

--- algorithms.h
+++ algorithms.h
@@ -2,7 +2,7 @@
  * @file        algorithms.h
  * @author      Bruno Pezer
  * @brief       Function definitions for algorithms described in Alan Bertossi's "Algoritmi e strutture dati".
- * @version     0.2
+ * @version     0.3
  * @date        2023-05-22
  *
  * @copyright   NO Copyright !(c) 2023
@@ -10,24 +10,21 @@
  */
 
 /**
- * @brief       Basic iterative algorithm for finding the minimum
- *              element of an array.
+ * @brief       Basic iterative algorithm for finding the minimum element of an array.
  *
- * @details     Uses a variable @var m of type integer.
- *              Considers the first array item @param a and assigns it to @var m.
- *              While @param a has items, consider the next item:
- *              if the next item is less than @var m, assign item to @var m.
+ * @details     Consider the first array item as the smallest.
+ *              For each item in the array consider the next item:
+ *              if it is less than the current smallest, consider this one the smallest.
  *
  * @param  a    Array to parse
  * @param  size Size of the array
- * @var    m    Minimum array element
- * @return m
+ * @return      m: the smallest item in the array
  */
 int min(int a[], int size) {
     //  If the array contains only one item, return it.
     if (size == 1) return a[0];
 
-    //  Consider the first item. If the next item is smaller, assign it to @var m.
+    //  Consider the first item. If the next item is smaller, assign it to m.
     int m = a[0];
     for (size_t i = 1; i < size; i++) {
         if (a[i] < m)
@@ -41,8 +38,8 @@ int min(int a[], int size) {
  *               element of an array. The minimum is either the first
  *               item or the minimum item in the rest of the array.
  *
- * @details      If the @param first is equal to @param last,
- *               the array contains one item, which is also the smallest.
+ * @details      If the first position is equal to the last position,
+ *               the array contains only one item which is also the smallest.
  *               Otherwise, the first item is considered to be the smallest,
  *               while the smallest item in the remainder of the array is
  *               recursively calculated. The smallest of these two values is returned.
@@ -50,20 +47,75 @@ int min(int a[], int size) {
  * @param  a     Array to parse
  * @param  first Position of the first item in the array
  * @param  last  Position of the last item in the array
- * @var    m     Minimum array element
- * @return m
+ * @return       m: the smallest item in the array
  */
-int recurmin(int a[], int first, int last) {
+int recurMin(int a[], int first, int last) {
     // If the array contains only one item, return it.
     if (first == last) {
         return a[first];
     } else {
-        //  Calculate the smallest item in the rest of the array
-        int m = recurmin(a, first + 1, last);
+        //  Calculate the smallest item in the rest of the array.
+        int m = recurMin(a, first + 1, last);
 
         //  Return the first item of the array, or the smallest from the remainder, whichever is smaller.
         if (a[first] < m) {
             return a[first];
         } else return m;
+    }
+}
+
+/**
+ * @brief       Auxiliary function for the sort algorithm implementation, returning the smallest item of an array.
+ *
+ * @details     Finds the smallest item of an array along with its position within the array.
+ *
+ * @param a     Array to parse
+ * @param j     Initial position of the array to parse
+ * @param size  Size of the provided array
+ * @param p     Pointer to the position of the smallest item within the array
+ *
+ * @return      m: the smallest item in the array
+ *
+ */
+int getMin(int a[], int j, int size, int *p) {
+    // If the array contains only one item, return it.
+    if (size == 1) return a[0];
+
+    int m;      // Smallest item
+    m = a[j];   //  Consider the initial item to be the smallest.
+    *p = j;     //  Point to the position of the initial item.
+
+    //  Consider each item after the initial one. If the next one is smaller,
+    //  update the smallest item and the pointer to its position accordingly.
+    for (size_t i = j + 1; i < size; i++) {
+        if (a[i] < m) {
+            m = a[i];
+            *p = i;
+        }
+    }
+    return m;
+}
+
+/**
+ * @brief      Sorts an array in ascending order.
+ *
+ * @details    Uses an auxiliary function to discover the smallest item in the array and places it
+ *             in the first position. Repeats the process for the remaining items
+ *             (starting from the second position, then the third, etc.) until the array is sorted.
+ *
+ * @param a    Array to sort
+ * @param size Size of the array
+ */
+void sort(int a[], int size) {
+    int pos;    //  Original position of the smallest item
+    int temp;   //  Holder variable for switching the current item with the smallest
+
+    //  Consider each item of the array. Store it in a temporary variable, and switch it
+    //  with the smallest item found. Repeat from the next position and the smallest
+    //  item remaining until the entire array has been sorted.
+    for (size_t i = 0; i < size; i++) {
+        temp = a[i];
+        a[i] = getMin(a, i, size, &pos);
+        a[pos] = temp;
     }
 }
--- debug/debug.c
+++ debug/debug.c
@@ -15,7 +15,13 @@ int main(void) {
         printf("%d ", a[i]);
     }
     printf("\n%s%d\n\n","The smallest element is ", min(a, size));
-    printf("\n%s%d\n\n","Recursively calculating the smallest element: ", recurmin(a, 0, size - 1));
+    printf("\n%s%d\n\n","Recursively calculating the smallest element: ", recurMin(a, 0, size - 1));
+
+    puts("Ordering array...");
+    sort(a, size);
+    for (size_t i = 0; i < size; i++) {
+        printf("%d ", a[i]);
+    }
 
     int bsize = 1;
     int b[1] = {666};
@@ -24,7 +30,7 @@ int main(void) {
         printf("%d ", b[i]);
     }
     printf("\n%s%d\n\n","The smallest element is ", min(b, bsize));
-    printf("\n%s%d\n\n","Recursively calculating the smallest element: ", recurmin(b, 0, bsize - 1));
+    printf("\n%s%d\n\n","Recursively calculating the smallest element: ", recurMin(b, 0, bsize - 1));
 
     return 0;
 }
